ef47a5bfae0559e69e8e3a5c1e9de19f88c27a37
Nurudin07 <udinkazama07@gmail.com>
2018-02-17 13:31:24 +0700

    Menambahkan line CMD

--- macho.php
+++ macho.php
@@ -12,6 +12,18 @@
                 <span>Strong</span>
                 <span>Strong</span>yguhjhnjnj
                 <span>Strong</span>
+                <div>
+                    remote: Counting objects: 6, done.
+                    remote: Compressing objects: 100% (4/4), done.
+                    remote: Total 6 (delta 2), reused 6 (delta 2), pack-reused 0
+                    Unpacking objects: 100% (6/6), done.
+                    From https://github.com/arjunane-tamvan/Strong
+                       a212b1a..7c38f76  master     -> origin/master
+                    Updating a212b1a..7c38f76
+                    Fast-forward
+                     macho.php | 6 ++++++
+                     1 file changed, 6 insertions(+)
+                </div>
             </div>
             <div>
                 <p>Mencoba isi paragraph</p>
