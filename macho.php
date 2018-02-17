48d2d2d415fb99af7514626f5ab6d55b9010efa0
arjunane-tamvan <arjunane.kajen@gmail.com>
2018-02-17 13:31:30 +0700

    menghapus

--- macho.php
+++ macho.php
@@ -8,25 +8,6 @@
     <body>
         <header>
             <div>
-                <span>Strong nan sangat oke kang</span>
-                <span>Strong</span>
-                <span>Strong</span>yguhjhnjnj
-                <span>Strong</span>
-                <div>
-                    remote: Counting objects: 6, done.
-                    remote: Compressing objects: 100% (4/4), done.
-                    remote: Total 6 (delta 2), reused 6 (delta 2), pack-reused 0
-                    Unpacking objects: 100% (6/6), done.
-                    From https://github.com/arjunane-tamvan/Strong
-                       a212b1a..7c38f76  master     -> origin/master
-                    Updating a212b1a..7c38f76
-                    Fast-forward
-                     macho.php | 6 ++++++
-                     1 file changed, 6 insertions(+)
-                </div>
-            </div>
-            <div>
-                <p>Mencoba isi paragraph</p>
             </div>
         </header>
     </body>
