a212b1a5f29a4561857ecc17548d0573033f6f0a
Nurudin07 <udinkazama07@gmail.com>
2018-02-17 13:20:55 +0700

    ubah

--- macho.php
+++ macho.php
@@ -7,7 +7,7 @@
             <div>
                 <span>Strong nan sangat oke kang</span>
                 <span>Strong</span>
-                <span>Strong</span>
+                <span>Strong</span>yguhjhnjnj
                 <span>Strong</span>
             </div>
         </header>
