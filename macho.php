5b9429166f81dd7bf058eed27b571e8decfecb92
arjunane-tamvan <arjunane.kajen@gmail.com>
2018-02-17 12:48:21 +0700

    add file macho.php

---
+++ macho.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>mencoba strong</title>
+    </head>
+    <body>
+        <header>
+            <div>
+                <span>Strong nan sangat oke kang</span>
+            </div>
+        </header>
+    </body>
+</html>
